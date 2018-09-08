84200874e1e7e4c8db4954d2a09f75c1e98c229d
Vlad Riscutia <vladris@outlook.com>
2018-09-08 16:04:05 -0700

    Document Never and Unit, build javadoc

--- src/com/vladris/poke/Unit.java
+++ src/com/vladris/poke/Unit.java
@@ -1,5 +1,14 @@
 package com.vladris.poke;
 
+/**
+ *
+ * Represents a unit type.
+ * 
+ * <p>
+ * A unit type has only one possible value. It is semantically equivalent to Void, but
+ * can be instantiated.
+ * </p>
+ */
 public final class Unit {
 	private Unit() { }
 
@@ -7,6 +16,11 @@ public final class Unit {
         public static final Unit instance = new Unit();
     }
 
+    /**
+     * Gets the singleton Unit value.
+     * 
+     * @return Singleton Unit value.
+     */
     public static Unit get() {
         return UnitHolder.instance;
     }
